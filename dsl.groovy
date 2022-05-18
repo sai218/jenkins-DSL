e18cc3599be72b971888b72e8ea9a5d8e4b03508
sai218 <kandavalli.saibabu@gmail.com>
2022-05-18 17:22:11 +0530

    pushing dsl grrovy

--- dsl.groovy
+++ dsl.groovy
@@ -1,9 +1,7 @@
-job('dsljob-1'){
-    steps{
-         git ('checkout([$class: 'GitSCM', branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/sai218/sai-login.git']]])')
+   job("job-dsl") {
+    scm {
+        git("https://github.com/sai218/sai-login.git", "master")
     }
-        
+   }
 
-}
 
-   
