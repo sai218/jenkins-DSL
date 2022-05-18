0c6adb30fcd568df40d25104cdfd4c487f696697
sai218 <kandavalli.saibabu@gmail.com>
2022-05-18 18:29:32 +0530

    pushing dsl grrovy

--- dsl.groovy
+++ dsl.groovy
@@ -1,7 +1,15 @@
    job("job-dsl") {
+    
     scm {
         git("https://github.com/sai218/sai-login.git", "master")
     }
+
+    steps{
+       sh 'mvn clean install '
    }
+
+    
+   }
+
 
 
